ea84ba39b652487fb494736d2fb91d83f2643529
JAZMIN ARIELA BEIGEL <47873291@ort>
2025-08-25 10:34:55 -0300

    agregado de reseñas

---
+++ Backend/services/resenas-service.js
@@ -0,0 +1,26 @@
+import ResenasRepository from '../repositories/Resenas-repository.js';
+
+export default class ResenasService {
+    constructor() {
+        console.log('ResenasService.constructor()');
+        this.ResenasRepository = new ResenasRepository();
+    }
+
+    getAllAsync = async () => {
+        console.log(`ResenasService.getAllAsync()`);
+        const returnArray = await this.ResenasRepository.getAllAsync();
+        return returnArray;
+    }
+
+    getByIdAsync = async (id) => {
+        console.log(`ResenasService.getByIdAsync(${id})`);
+        const returnEntity = await this.ResenasRepository.getByIdAsync(id);
+        return returnEntity;
+    }
+
+    createAsync = async (entity) => {
+        console.log(`ResenasService.createAsync(${JSON.stringify(entity)})`);
+        const rowsAffected = await this.ResenasRepository.createAsync(entity);
+        return rowsAffected;
+    }
+}
